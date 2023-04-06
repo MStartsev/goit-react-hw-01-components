dec44d812b9b76c5ae4a5192dccf4768ef0a7cb0
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-06 06:10:03 +0300

    task 3

--- src/components/friendList/FriendListItem/FriendListItem.jsx
+++ src/components/friendList/FriendListItem/FriendListItem.jsx
@@ -1,23 +1,12 @@
 import css from './FriendListItem.module.css';
 
-const FriendListItem = ({ friends }) => {
-  return friends.map(items => {
-    const { id, avatar, name, isOnline } = items;
-    console.log(id, name, isOnline);
-    return (
-      <li key={id} className={css.item}>
-        <span className={isOnline ? css.online : css.offline}></span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-      </li>
-    );
-  });
-};
+const FriendListItem = ({ friends }) =>
+  friends.map(({ id, avatar, name, isOnline }) => (
+    <li key={id} className={css.item}>
+      <span className={isOnline ? css.online : css.offline}></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  ));
 
 export default FriendListItem;
-
-/* <li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li>; */
